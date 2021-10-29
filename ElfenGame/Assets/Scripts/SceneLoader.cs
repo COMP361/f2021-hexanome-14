f2f2625e033e1290c45e1d6aa7a8e9ba5b21df14
Emmanuel Ma <emmanuel.ma@gmail.com>
2021-10-28 21:45:07 -0400

    create login

--- ElfenGame/Assets/Scripts/SceneLoader.cs
+++ ElfenGame/Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
